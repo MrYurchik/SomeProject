9be778afdee5d2d47d87c5606dc1e7b344e97dd1
YuriiShik <yur4ik322@gmail.com>
2023-05-22 19:20:08 +0300

    first commit

---
+++ compress_the_string.py
@@ -0,0 +1,21 @@
+def compress_the_string(number: int):
+    if not str(number).isdigit():
+        raise ValueError('Number must be integer')
+    count = 0
+    number = list(map(int, str(number)))
+    past = number[0]
+    result = []
+    for num in number:
+        if num != past:
+            result.append([count, past])
+            count = 1
+        else:
+            count += 1
+        past = num
+    result.append([count, past])
+    return result
+
+
+# if __name__ == '__main__':
+#     compress_the_string(a)
+#     compress_the_string('asd')
---
+++ main.py
@@ -0,0 +1,54 @@
+import json
+
+import uvicorn
+from fastapi import FastAPI, Request, Form
+from starlette.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from typing import Optional
+
+from compress_the_string import compress_the_string
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/get_problem/{problem_name}", response_class=HTMLResponse)
+@app.get("/get_problem/", response_class=HTMLResponse)
+async def get_problem(request: Request, problem_name: Optional[str] = None):
+    print(f"Problem name {problem_name}")
+    with open("config.json", "r+") as file:
+        content = json.loads(file.read())
+
+    if problem_name:
+        print('PROBLEM NAME ')
+        if problem_name in content:
+            return templates.TemplateResponse("item.html", {"request": request, "id": problem_name, "items": content[problem_name]})
+        else:
+            return templates.TemplateResponse("error.html", {"request": request, "error_massage": f"No course wih name {problem_name}.\n All available names {list(content.keys())}"})
+
+    else:
+        return templates.TemplateResponse("item.html", {"request": request, "id": problem_name, "items": []})
+
+@app.get("/view_problem/{problem_id}", response_class=HTMLResponse)
+async def view_problem(request: Request, problem_id: int):
+    with open("config.json", "r+") as file:
+        content = json.loads(file.read())
+        # тимчасова затичка під одну задачу
+        content = content["Pythonist 3"][4]
+    return templates.TemplateResponse("problem_solver.html", {"request": request, "problem_id": problem_id, "problem": content})
+
+
+@app.post("/submit")
+async def submit(user_input: int = Form(...)):
+    return {"solution of the problem": compress_the_string(user_input)}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
